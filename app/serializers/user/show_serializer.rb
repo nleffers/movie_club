8a7024fbd80abf68573fde08a780ae754bd07323
nleffers <nickleffers@gmail.com>
2020-07-19 22:44:05 -0600

    added token to serializer

--- app/serializers/user/show_serializer.rb
+++ app/serializers/user/show_serializer.rb
@@ -5,7 +5,8 @@ class User::ShowSerializer < ActiveModel::Serializer
              :first_name,
              :last_name,
              :email,
-             :email_notifications
+             :email_notifications,
+             :token
 
   has_many :movies, through: :user_movies, each_serializer: Movie::UserSerializer
   has_many :reviews, each_serializer: Review::UserSerializer
