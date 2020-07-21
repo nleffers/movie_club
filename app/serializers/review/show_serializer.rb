53ac853f4d9dffe3d691a16d996d432a1ed6466b
nleffers <nickleffers@gmail.com>
2020-07-21 01:18:58 -0600

    added to serializer

--- app/serializers/review/show_serializer.rb
+++ app/serializers/review/show_serializer.rb
@@ -1,6 +1,11 @@
 # Responsible for serializing a Review
 class Review::ShowSerializer < ActiveModel::Serializer
   attributes :id,
+             :written_by_username,
              :title,
              :blog
+
+  def written_by_username
+    User.find_by(id: object.user_id)&.username
+  end
 end
