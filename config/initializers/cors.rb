216b69cc4d9c80570185affae09342dff153e2ed
nleffers <nickleffers@gmail.com>
2020-07-30 06:21:16 -0600

    allowing everything

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ if Rails.env.development?
 else
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://immense-mountain-08471.herokuapp.com/*'
+      origins '*'
 
       resource '*',
                headers: :any,
